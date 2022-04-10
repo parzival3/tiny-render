d895f01d17d80e496c4782c6b8d6c0bd618b9a59
Enrico Tolotto <etolotto@gmail.com>
2022-04-10 22:04:30 +0200

    tst/formula-tests:cpp: empty the file

--- tst/formula-tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "formula.hpp"
-#include "tests_include.hpp"
-
-TEST(blaTest, test1) {
-  math::Formula formula;
-  EXPECT_EQ(formula.identity(0), 0);
-  EXPECT_EQ(formula.identity(10), 10);
-  EXPECT_EQ(formula.identity(50), 50);
-}
