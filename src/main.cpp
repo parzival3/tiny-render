e4f8d8d3e4b56bbee6c0d62ba329a119c5d6e8b0
Enrico Tolotto <etolotto@gmail.com>
2022-04-10 22:02:08 +0200

    src/main:cpp: update with triangle render

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
-#include "formula.hpp"
 #include <iostream>
+#include <optional>
+#include <stdexcept>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-  math::Formula formula;
-  return formula.identity(0) == 0;
+#include "model.hpp"
+#include "tga-lib/tgaimage.hpp"
+#include "tiny-render.hpp"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+//static constexpr int width_line{800};
+//static constexpr int height_line{800};
+
+static constexpr int width{200};
+static constexpr int height{200};
+
+int main(int argc, char** argv) {
+  std::optional<tr::Model> model;
+  if (argc == 2) {
+    model = tr::Model(argv[1]);
+  } else {
+    model = tr::Model("obj/african_head.obj");
+  }
+
+  if (!model) {
+    throw std::runtime_error("Error: couldn't load the image model");
+  }
+
+  std::string image_name{"output.tga"};
+
+  // tr::line_render(width_line, height_line, *model, white, image_name);
+  tr::triangle_render(width, height, white, image_name);
+  std::cout << "Writing image " << image_name << " ...\n";
+  return 0;
 }
