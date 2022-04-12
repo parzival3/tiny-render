06014bc9f3922274d2d95ce839219f640d1996af
Enrico Tolotto <etolotto@gmail.com>
2022-04-12 17:19:21 +0200

    src/tiny-render:hpp: add mono chrome light example

--- src/tiny-render.hpp
+++ src/tiny-render.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <optional>
 
 #include "geometry.hpp"
 #include "model.hpp"
@@ -19,6 +20,23 @@ void triangle(const Vec2i& t_0, const Vec2i& t_1, const Vec2i& t_2,
 void line_render(int width, int height, const tr::Model& model,
                  const TGAColor& color, const std::string& tga_filename);
 
-void triangle_render(int width, int height, const TGAColor& color,
-                     const std::string& tga_filename);
+void triangle_render_0(int width, int height, const TGAColor& color,
+                       const std::string& tga_filename);
+
+void triangle_render_1(int width, int height, const TGAColor& color,
+                       const std::string& tga_filename);
+
+void triangle(const std::array<Vec2i, 3>& pts, TGAImage& image,
+              const TGAColor& color);
+
+void color_obj(int width, int height, const tr::Model& model,
+               const std::string& tga_filename);
+
+std::optional<Vec3f> barycentric(const std::array<Vec2i, 3>& pts,
+                                 const Vec2i& p);
+
+std::array<Vec2i, 2> get_bbox(const std::array<Vec2i, 3>& pts, TGAImage& image);
+
+void color_obj(TGAImage& image, const Model& model);
+void light_mono_obj(TGAImage& image, const Model& model);
 }  // namespace tr
