b4fcf293f93e3d88b39d3da17ad376871f08df30
Enrico Tolotto <etolotto@gmail.com>
2022-04-12 17:20:39 +0200

    src/model:hpp: rename methods

--- src/model.hpp
+++ src/model.hpp
@@ -10,8 +10,8 @@ namespace tr {
 class Model {
  public:
   Model(const std::string& filename);
-  [[nodiscard]] size_t verts_number() const;
-  [[nodiscard]] size_t faces_number() const;
+  [[nodiscard]] size_t verts_size() const;
+  [[nodiscard]] size_t faces_size() const;
   [[nodiscard]] Vec3f vert(size_t i) const;
   [[nodiscard]] std::vector<size_t> face(size_t i) const;
 
