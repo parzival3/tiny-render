6b0b2bff147f53be9452c1774ded1d272f5c13ff
Enrico Tolotto <etolotto@gmail.com>
2022-04-14 10:19:56 +0200

    src/main:cpp: format file

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,22 @@ static constexpr int width{800};
 static constexpr int height{800};
 
 int main(int argc, char** argv) {
-  std::optional<tr::Model> model;
-  if (argc == 2) {
-    model = tr::Model(argv[1]);
-  } else {
-    model = tr::Model("obj/african_head.obj");
-  }
+    std::optional<tr::Model> model;
+    if (argc == 2) {
+        model = tr::Model(argv[1]);
+    } else {
+        model = tr::Model("obj/african_head.obj");
+    }
 
-  if (!model) {
-    throw std::runtime_error("Error: couldn't load the image model");
-  }
+    if (!model) {
+        throw std::runtime_error("Error: couldn't load the image model");
+    }
 
-  std::string image_name_0{"output_0.tga"};
+    std::string image_name_0{"output_0.tga"};
 
-  TGAImage image_0(width, height, TGAImage::RGB);
-  tr::light_mono_obj(image_0, *model);
-  std::cout << "Writing image " << image_name_0 << " ...\n";
-  image_0.write_tga_file(image_name_0);
-  return 0;
+    TGAImage image_0(width, height, TGAImage::RGB);
+    tr::light_mono_obj(image_0, *model);
+    std::cout << "Writing image " << image_name_0 << " ...\n";
+    image_0.write_tga_file(image_name_0);
+    return 0;
 }
