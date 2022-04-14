b46a8d5ece5b36c65a1b51b5305b7c5a495f6ebd
Enrico Tolotto <etolotto@gmail.com>
2022-04-14 10:21:48 +0200

    src/model:hpp: update methods

--- src/model.hpp
+++ src/model.hpp
@@ -8,16 +8,18 @@
 namespace tr {
 
 class Model {
- public:
-  Model(const std::string& filename);
-  [[nodiscard]] size_t verts_size() const;
-  [[nodiscard]] size_t faces_size() const;
-  [[nodiscard]] Vec3f vert(size_t i) const;
-  [[nodiscard]] std::vector<size_t> face(size_t i) const;
+   public:
+    Model(const std::string& filename);
+    [[nodiscard]] size_t verts_size() const;
+    [[nodiscard]] size_t faces_size() const;
+    [[nodiscard]] Vec3f vert(size_t index) const;
+    [[nodiscard]] std::vector<size_t> face(size_t index) const;
+    [[nodiscard]] std::array<Vec3f, 3> face_coords(size_t index) const;
+    [[nodiscard]] std::vector<std::vector<size_t>> faces() const { return faces_; }
 
- private:
-  std::vector<Vec3f> verts_;
-  std::vector<std::vector<size_t>> faces_;
+   private:
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<size_t>> faces_;
 };
 
 }  // namespace tr
